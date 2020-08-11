23926bbf04d141a006ecf3251ff35d065e362d60
[Google Jr] <andilembele020@gmail.com>
2020-08-11 13:14:38 +0200

    Let's check ouy Objects

---
+++ js/fundamentals/assets/js/objects.js
@@ -0,0 +1,14 @@
+//Let's se JS Objects 
+// NB: In Javascrip,functions are objects, functions can be passed asarguments
+
+// An object is abuilt-in data type for storing key-value pairs. Data inside objects are unordered, and can be of any type.author
+
+const classOf2020 = {
+    students: 50,
+    year: 2020
+};
+
+const persona = {
+    firstName: 'Andile',
+    lastName: 'Mbele'
+}
