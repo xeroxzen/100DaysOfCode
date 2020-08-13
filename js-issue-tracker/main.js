7935b9af6918a70ac0b6cd63e84e2c2286edd4a8
[Google Jr] <andilembele020@gmail.com>
2020-08-13 23:11:14 +0200

    Almost finished

--- js-issue-tracker/main.js
+++ js-issue-tracker/main.js
@@ -1,5 +1,68 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+	var issueDesc = document.getElementById('issueDescInput').value;
+	var issueSeverity = document.getElementById('issueSeverityInput').value;
+	var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+	var issued = chance.guid();
+	var issueStatus = 'Open';
+
+	var issue = {
+		id: issueId,
+		description: issueDesc,
+		severity: issueSeverity,
+		assignedTo: issueAssignedTo,
+		status: issueStatus
+	}
+
+	if (localStorage.getItem('issues') == null) {
+		var issues = [];
+		issues.push(issue);
+		localStorage.setItem('issues', JSON.stringify(issues));
+	} else {
+		var issues = JSON.parse(localStorage.getItem('issues'));
+		issues.push(issue);
+		localStorage.setItem('issues', JSON.stringify(issues));
+	}
+
+	document.getElementById('issueInputForm').reset();
+
+	fetchIssues();
+
+	e.preventDefault();
+}
+
+function setStatusClosed(id) {
+	var issues = JSON.parse(localStorage.getItem('issues'));
+
+	for (var i = 0; i < issues.length; i++) {
+		if (issues[i].id == id) {
+
+			issues[i].status = 'Closed';
+		}
+	}
+
+	localStorage.setItem('issues', JSON.stringify(issues));
+
+	fetchIssues();
+}
+
+function deleteIssue(id) {
+	var issues = JSON.parse(localStorage.getItem('issues'));
+
+	for (var i = 0; i < issues.length; i++) {
+		if (issues[i].id) {
+
+			issues[i].status = 'Closed';
+		}
+	}
+	localStorage.setItem('issues', JSON.stringify(issues));
+
+	fetchIssues();
+} 			
+
 function fetchIssues() {
-	const issues = JSON.parse(localStorage.getItem('issues'));
+	var issues = JSON.parse(localStorage.getItem('issues'));
 	const issuesList = document.getElementById('issuesList');
 
 	issuesList.innerHTML = '';
@@ -11,11 +74,14 @@ function fetchIssues() {
 		var assignedTo = issues[i].assignedTo;
 		var status = issues[i].status;
 
-		issuesList.innerHTML += '<div class="well">' + '<h6>Issue ID: ' + id + '</h6>' + '<p><span class="label label-info">' + status + '</span></p>' + '<h3>' + desc + '</h3>' +
-		'<p><span class="glyphicon-time"></span>' + severity + '</p>' +
-		'<p><span class="glyphicon-user"></span>' + assignedTo + '</p>' +
-		'<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
-		'<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-delete">Delete</a>' +
-		'</div>';
+		issuesList.innerHTML += '<div class="well">' +
+								'<h6>Issue ID: ' + id + '</h6>' +
+								'<p><span class="label label-info">' + status + '</span></p>' +
+								'<h3>' + desc + '</h3>' +
+								'<p><span class="glyphicon-time"></span>' + severity + '</p>' +
+								'<p><span class="glyphicon-user"></span>' + assignedTo + '</p>' +
+								'<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
+								'<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-delete">Delete</a>' +
+								'</div>';
 	}
 }
