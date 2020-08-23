52f83c9cd27746755aa3c09ad49ea6308e14587d
xeroxzen <andilembele020@gmail.com>
2020-08-23 19:03:31 +0200

    Let's deal with the frontend

---
+++ mern-starter/frontend/src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+
+ReactDOM.render(<App />, container);
